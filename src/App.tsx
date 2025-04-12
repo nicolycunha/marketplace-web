f93a46a2c2a883dacc74bcd679c6396bd93bfe16
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-04-12 15:05:04 -0300

    feat: create font variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline font-dm-sans">
+        Hello world!
+      </h1>
       <button className="bg-orange-base text-white px-4 py-2 rounded">
         Clique aqui
       </button>
