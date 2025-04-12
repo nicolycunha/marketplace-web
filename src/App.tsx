fb390d1f534bb44617ef2ea2ca795712002e4037
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-04-12 14:28:05 -0300

    feat: create colors variables

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-orange-base text-white px-4 py-2 rounded">
+        Clique aqui
+      </button>
+    </>
+  )
+}
